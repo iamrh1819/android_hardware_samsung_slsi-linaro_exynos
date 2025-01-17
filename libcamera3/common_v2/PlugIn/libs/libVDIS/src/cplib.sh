6cd06493d0e7ebf4e2d7f9012db6eba33e95e1e1
Stricted <info@stricted.net>
2025-01-17 10:44:53 +0100

    exynos: remove old unused code
    Change-Id: If8d69ba21c0d0856320603fb32fe4aa87d22fb02

--- libcamera3/common_v2/PlugIn/libs/libVDIS/src/cplib.sh
+++
@@ -1,2 +0,0 @@
-cp ~/work/s-o-ww-stable/out/target/product/maestro9610/vendor/lib/libvdis.so ../lib32/
-cp ~/work/s-o-ww-stable/out/target/product/maestro9610/vendor/lib64/libvdis.so ../lib64/
--- libvision/kernel/opencl/cl/vx_error.cl
+++
@@ -1,6 +0,0 @@
-
-__kernel void vx_myfunc(__global void *ptr) {
-    //printf("Recevied Ptr %p\n", ptr);
-}
-
-
